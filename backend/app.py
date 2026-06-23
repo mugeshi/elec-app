b4e32c115a425f2c7a52bc6cd5d8b9c113029727
mugeshi <nicolemugeshi@gmail.com>
2026-06-23 08:02:04 +0300

    development of Fron-end

--- backend/app.py
+++ backend/app.py
@@ -1,27 +1,64 @@
-from flask import Flask, jsonify
-from flask_sqlalchemy import SQLAlchemy
+from flask import Flask, request, jsonify
+from model import db, Product
 
-# Creating the Flask Application
 app = Flask(__name__)
 
-# Database Configuration
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///product.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///products.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
-db = SQLAlchemy(app)
+db.init_app(app)
 
-# Create tables
 with app.app_context():
     db.create_all()
 
+
 @app.route('/products', methods=['GET'])
-def get_product():
-    products = Product.query.all()
-    return jsonify([product.serialize() for product in products])
+def get_products():
+    return jsonify([p.serialize() for p in Product.query.all()])
+
+
+@app.route('/products', methods=['POST'])
+def add_product():
+    data = request.get_json()
+
+    product = Product(
+        name=data['name'],
+        description=data.get('description', ''),
+        price=data['price']
+    )
+
+    db.session.add(product)
+    db.session.commit()
+
+    return jsonify(product.serialize()), 201
+
+
+@app.route('/products/<int:id>', methods=['PUT'])
+def update_product(id):
+    product = Product.query.get_or_404(id)
+    data = request.get_json()
+
+    product.name = data['name']
+    product.description = data.get('description', '')
+    product.price = data['price']
+
+    db.session.commit()
+
+    return jsonify(product.serialize())
+
+
+@app.route('/products/<int:id>', methods=['DELETE'])
+def delete_product(id):
+    product = Product.query.get_or_404(id)
+
+    db.session.delete(product)
+    db.session.commit()
+
+    return jsonify({'message': 'Deleted'})
+
 
 if __name__ == '__main__':
     app.run(debug=True)
-
 
 
 
---
+++ frontend/src/App.js
@@ -0,0 +1,15 @@
+import REact from 'react';
+
+function App()  {
+  return (
+  <div className='app'>
+    <Navbar />
+    <HeroSecction />
+    <Categories />
+    <WhyChooseUs />
+    <FeaturedProduct/>
+  </div>
+  );
+}
+
+export default App;
