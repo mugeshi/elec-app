826ac9e09813ac500161a0136d045a182b51bd51
mugeshi <nicolemugeshi@gmail.com>
2026-07-01 22:42:35 +0300

    updated navbar

--- frontend/src/Components/Navbar.js
+++ frontend/src/Components/Navbar.js
@@ -8,12 +8,32 @@ function Navbar() {
       </div>
 
       <ul className="nav-links">
-        <li><a href="#home">Home</a></li>
-        <li><a href="#iphone">iPhone</a></li>
-        <li><a href="#samsung">Samsung</a></li>
-        <li><a href="#accessories">Accessories</a></li>
-        <li><a href="#sell">Sell Phone</a></li>
-        <li><a href="#contact">Contact</a></li>
+        <li><a href="/">Home</a></li>
+
+        <li className="dropdown">
+          <a href="/phones">Phones</a>
+          <ul className="dropdown-menu">
+            <li><a href="/phones/apple">Apple</a></li>
+            <li><a href="/phones/samsung">Samsung</a></li>
+            <li><a href="/phones/google">Google Pixel</a></li>
+            <li><a href="/phones/nothing">Nothing Phone</a></li>
+            <li><a href="/phones/oneplus">OnePlus</a></li>
+            <li><a href="/phones/xiaomi">Xiaomi</a></li>
+            <li><a href="/phones/oppo">Oppo</a></li>
+            <li><a href="/phones/vivo">Vivo</a></li>
+            <li><a href="/phones/huawei">Huawei</a></li>
+          </ul>
+        </li>
+
+        <li><a href="/laptops">Laptops</a></li>
+
+        <li><a href="/accessories">Accessories</a></li>
+
+        <li><a href="/sell-phone">Sell Your Device</a></li>
+
+        <li><a href="/about">About</a></li>
+
+        <li><a href="/contact">Contact</a></li>
       </ul>
     </nav>
   );
